542b77639adae4912e704ddb13b67cce44d8d674
Nikhilareddy57 <nikhilareddy57@gmail.com>
2019-09-03 08:02:29 +0530

    indented code

--- client.java
+++ client.java
@@ -7,18 +7,14 @@ class TCPClient {
         String sentence; 
         String modifiedSentence; 
 
-        BufferedReader inFromUser = 
-          new BufferedReader(new InputStreamReader(System.in)); 
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
 
         Socket clientSocket = new Socket("localhost", 6789); 
 
-        DataOutputStream outToServer = 
-          new DataOutputStream(clientSocket.getOutputStream()); 
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); 
 
         
-        BufferedReader inFromServer = 
-          new BufferedReader(new
-          InputStreamReader(clientSocket.getInputStream())); 
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
 
 		OutputToServer outputThread = new OutputToServer(clientSocket, outToServer, inFromUser);
 		InputFromServer inputThread = new InputFromServer(clientSocket, inFromServer);
@@ -33,60 +29,59 @@ class TCPClient {
 } 
 
 class InputFromServer implements Runnable {
-     String serverSentence; 
-     Socket connectionSocket;
-     BufferedReader inFromServer;
+    String serverSentence; 
+    Socket connectionSocket;
+    BufferedReader inFromServer;
    
-     InputFromServer (Socket connectionSocket, BufferedReader inFromServer) {
-		this.connectionSocket = connectionSocket;
+    InputFromServer (Socket connectionSocket, BufferedReader inFromServer) {
+        this.connectionSocket = connectionSocket;
         this.inFromServer = inFromServer;
-		System.out.println("Constructor check ./");
-     } 
+        System.out.println("Constructor check ./");
+    } 
 
-     public void run() {
-       while(true) { 
-	   try {
-
-	           serverSentence = inFromServer.readLine();
-				if(serverSentence != null)
-					System.out.println("From Server: "+serverSentence);
-	   } catch(Exception e) {
-		try {
-			connectionSocket.close();
-		} catch(Exception ee) { }
-		System.out.println(e);
-		break;
-	   }
+    public void run() {
+        while(true) { 
+            try {
+                serverSentence = inFromServer.readLine();
+                if(serverSentence != null)
+                    System.out.println("From Server: "+serverSentence);
+            } 
+            catch(Exception e) {
+                try {
+                    connectionSocket.close();
+                } catch(Exception ee) { }
+                System.out.println(e);
+                break;
+            }
         } 
     }
 }
 
 class OutputToServer implements Runnable {
-     String clientSentence; 
-     Socket connectionSocket;
-     DataOutputStream outToServer;
-	 BufferedReader inFromUser;
+    String clientSentence; 
+    Socket connectionSocket;
+    DataOutputStream outToServer;
+    BufferedReader inFromUser;
    
-     OutputToServer (Socket connectionSocket, DataOutputStream outToServer, BufferedReader inFromUser) {
-		this.connectionSocket = connectionSocket;
+    OutputToServer (Socket connectionSocket, DataOutputStream outToServer, BufferedReader inFromUser) {
+        this.connectionSocket = connectionSocket;
         this.outToServer = outToServer;
-		this.inFromUser = inFromUser;
-     } 
+        this.inFromUser = inFromUser;
+    } 
 
-     public void run() {
-       while(true) {
-	   try {
-			   
-	           clientSentence = inFromUser.readLine();
-			   if(clientSentence != null)
-				   outToServer.writeBytes(clientSentence + '\n');
-	   } catch(Exception e) {
-		try {
-			connectionSocket.close();
-		} catch(Exception ee) { }
-		System.out.println("outputToServer "+e);
-		break;
-	   }
+    public void run() {
+        while(true) {
+            try {
+                clientSentence = inFromUser.readLine();
+                if(clientSentence != null)
+                    outToServer.writeBytes(clientSentence + '\n');
+            } catch(Exception e) {
+                try {
+                    connectionSocket.close();
+                } catch(Exception ee) { }
+                System.out.println("outputToServer "+e);
+                break;
+            }
         } 
     }
 }
--- server.java
+++ server.java
@@ -2,37 +2,30 @@ import java.io.*;
 import java.net.*; 
 
 class TCPServer { 
+    public static void main(String argv[]) throws Exception { 
+        String clientSentence; 
+        String capitalizedSentence; 
+        System.out.println("6789");
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+        // ServerSocket welcomeSocket2 = new ServerSocket(6789);
+        Socket connectionSocket1 = welcomeSocket.accept();
 
-  public static void main(String argv[]) throws Exception 
-    { 
-      String clientSentence; 
-      String capitalizedSentence; 
-	  System.out.println("6789");
-      ServerSocket welcomeSocket = new ServerSocket(6789);
-	  // ServerSocket welcomeSocket2 = new ServerSocket(6789);
-	  
-  
-      Socket connectionSocket1 = welcomeSocket.accept();
-
-      BufferedReader inFromClient1 = 
-           new BufferedReader(new
-           InputStreamReader(connectionSocket1.getInputStream()));
-	  System.out.println("connection 1 formed");
-	  // BufferedReader inFromClient2 = 
-           // new BufferedReader(new
-           // InputStreamReader(connectionSocket2.getInputStream()));
+        BufferedReader inFromClient1 = new BufferedReader(new InputStreamReader(connectionSocket1.getInputStream()));
+        System.out.println("connection 1 formed");
+        // BufferedReader inFromClient2 = 
+        // new BufferedReader(new
+        // InputStreamReader(connectionSocket2.getInputStream()));
 
 
-      // DataOutputStream  outToClient1 = 
-           // new DataOutputStream(connectionSocket1.getOutputStream());
-	  Socket connectionSocket2 = welcomeSocket.accept();
-	  DataOutputStream  outToClient2 = 
-           new DataOutputStream(connectionSocket2.getOutputStream());
-	  System.out.println("connection 2 formed");
-       while(true) { 
-           clientSentence = inFromClient1.readLine(); 
+        // DataOutputStream  outToClient1 = 
+        // new DataOutputStream(connectionSocket1.getOutputStream());
+        Socket connectionSocket2 = welcomeSocket.accept();
+        DataOutputStream  outToClient2 = new DataOutputStream(connectionSocket2.getOutputStream());
+        System.out.println("connection 2 formed");
+        while(true) { 
+            clientSentence = inFromClient1.readLine(); 
 
-			if(clientSentence != null){
+            if(clientSentence != null){
 			   System.out.println(clientSentence);
 			   capitalizedSentence = clientSentence.toUpperCase() + '\n'; 
 
